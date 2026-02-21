8e8508cc3bbf057c46056725bc8bca990032eaad
siri <ksiric126@gmail.com>
2026-02-21 02:55:06 -0600

    first commit

---
+++ src/main/java/com/cinefy/backend/CinefyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cinefy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinefyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cinefy/backend/CinefyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinefy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
